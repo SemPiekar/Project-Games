d16611e5d3d330911687c4533361fbadf95354f7
SemPiekar <9018659@student.zadkine.nl>
2022-10-31 21:51:49 +0100

    Favo Game Sem Added

--- sem.php
+++ sem.php
@@ -5,8 +5,14 @@ include_once 'header.php'
 <main class="main__student">
     <section>
         <h2 class="favo__header">Favo Game</h2>
-        <img src="" alt="Favo Game picture">
-        <p>Favo game</p>
+        <div class="favo__container">
+            <img class="favo__picture" src="images/StateOfDecay2.jpg" alt="Favo Game picture">
+            <p class="favo__text">
+                State of Decay 2 is een zombie-overlevingsspel, met de nadruk op het zoeken naar items, waarin de gameplay wordt ervaren vanuit een derde persoon.
+                De game speelt zich af in een open wereldomgeving en biedt coöperatieve gameplay met maximaal drie andere spelers.
+                De speler kan kiezen uit verschillende locaties om een ​​basis te bouwen en deze vervolgens te versterken en te verbeteren met verschillende faciliteiten zoals wachttorens, tuinen, slaapvertrekken, keukens, werkplaatsen, medische ruimtes, enz. om de overlevenden veilig en gezond te houden. Een deel van het spel is het balanceren van het gebruik van hulpbronnen: voedsel, medicijnen, munitie en bouwmaterialen. Ze kunnen worden verkregen door op zoek te gaan naar of te handelen met NPC's. De speler kan communiceren met overlevenden buiten hun groep: met hen handelen, hen helpen of rekruteren. Er kan slechts één overlevende tegelijk worden bestuurd, hoewel de speler een AI-gestuurde overlevende kan vragen om hem te vergezellen, en in bepaalde missies zullen een of meer AI-gestuurde overlevenden de speler vergezellen. Coöperatieve multiplayer is ook beschikbaar, zodat spelers een vriend kunnen rekruteren om tijdelijk deel te nemen aan hun spel om hen te helpen met doelen.
+            </p>
+        </div>
     </section>
     <section>
         <h2 class="favo__header">Game</h2>
